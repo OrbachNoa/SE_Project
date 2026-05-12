bd849d9b576a3f8ac0b98e79bd14baeee30b4b6f
Noa <orbachnoa123@gmail.com>
2026-05-12 12:08:03 +0300

    update progarms in english

--- data/programs.py
+++ data/programs.py
@@ -2,14 +2,14 @@
 Academic programs available in the system.
 """
 programs_data = {
-    83101: "הנדסת מחשבים",
-    83102: "הנדסת חשמל",
-    83104: "הנדסת תעשיה ומערכות מידע",
-    83107: "הנדסת נתונים",
-    83108: "הנדסת תוכנה",
-    83109: "הנדסת חומרים",
-    83105: "הנדסת מחשבים – מגמת חומרת מחשבים",
-    83182: "הנדסת חשמל – מגמת הנדסה קוונטית",
-    83103: "הנדסת חשמל – מגמת נוירו הנדסה",
-    83115: "הנדסת חשמל – מגמת הנדסה ביו-רפואית"
+    83101: "Computer Engineering",
+    83102: "Electrical Engineering",
+    83104: "Industrial and Information Systems Engineering",
+    83107: "Data Engineering",
+    83108: "Software Engineering",
+    83109: "Materials Engineering",
+    83105: "Computer Engineering - Computer Hardware Track",
+    83182: "Electrical Engineering - Quantum Engineering Track",
+    83103: "Electrical Engineering - Neuroengineering Track",
+    83115: "Electrical Engineering - Biomedical Engineering Track"
 }
